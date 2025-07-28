2a99ee19f3eb5c16017f919db8c8091561c89829
Bruno Nitro5 <carecsnay1@gmail.com>
2025-07-28 15:24:23 -0300

    chore: tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "Estudar React",
+            description: "28 de julho de 2025 às 10:00",
+            isCompleted: true,
+        },
+        {
+            id: 2,
+            text: "Fazer compras",
+            description: "29 de julho de 2025 às 14:30",
+            isCompleted: false,
+        },
+        {
+            id: 3,
+            text: "Reunião com o time",
+            description: "30 de julho de 2025 às 09:00",
+            isCompleted: true,
+        },
+        {
+            id: 4,
+            text: "Ir à academia",
+            description: "31 de julho de 2025 às 18:00",
+            isCompleted: false,
+        },
+    ]);
     return (
-        <div>
-            <h1>Gerenciador de Tarefas</h1>
-            <Tasks />
+        <div className="w-screen h-screen bg-slate-400 flex items-center justify-center flex-col p-6">
+            <div className="w-[500px]">
+                <h1 className="text-3xl text-white font-bold text-center">Gerenciador de Tarefas</h1>
+                <Tasks />
+            </div>
         </div>
     );
 }
