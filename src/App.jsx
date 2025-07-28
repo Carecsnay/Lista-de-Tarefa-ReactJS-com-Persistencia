66461d03125f2a10e3b0575699eb8ecf66799706
Bruno Nitro5 <carecsnay1@gmail.com>
2025-07-28 15:54:55 -0300

    feat: first style of application.

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,34 @@ function App() {
     const [tasks, setTasks] = useState([
         {
             id: 1,
-            text: "Estudar React",
+            title: "Estudar React",
             description: "28 de julho de 2025 às 10:00",
             isCompleted: true,
         },
         {
             id: 2,
-            text: "Fazer compras",
+            title: "Fazer compras",
             description: "29 de julho de 2025 às 14:30",
             isCompleted: false,
         },
         {
             id: 3,
-            text: "Reunião com o time",
+            title: "Reunião com o time",
             description: "30 de julho de 2025 às 09:00",
             isCompleted: true,
         },
         {
             id: 4,
-            text: "Ir à academia",
+            title: "Ir à academia",
             description: "31 de julho de 2025 às 18:00",
             isCompleted: false,
         },
     ]);
     return (
         <div className="w-screen h-screen bg-slate-400 flex items-center justify-center flex-col p-6">
-            <div className="w-[500px]">
-                <h1 className="text-3xl text-white font-bold text-center">Gerenciador de Tarefas</h1>
-                <Tasks />
+            <div className="w-[500px] bg-slate-500 py-10 p-4 rounded-xl">
+                <h1 className="text-3xl text-black font-bold text-center pb-4">Gerenciador de Tarefas</h1>
+                <Tasks tasks={tasks} />
             </div>
         </div>
     );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,14 @@
-function Tasks() {
+function Tasks(props) {
     return (
         <div>
-            <p>Tasks</p>
+            <ul>
+                {props.tasks.map((tasks) => (
+                    <li className="bg-slate-300 text-gray-700 p-2 rounded-md m-4 font-semibold" key={tasks.id}>
+                        {" "}
+                        {tasks.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
