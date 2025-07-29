9f625bf7f72a513a8fe5a40c38eeee0134bcd9dc
Bruno Nitro5 <carecsnay1@gmail.com>
2025-07-29 09:03:06 -0300

    feat: add new components (trash and chevronRight) and add function.

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,55 @@ import { useState } from "react";
 import Tasks from "./components/Tasks";
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: "Estudar React",
-            description: "28 de julho de 2025 às 10:00",
-            isCompleted: true,
-        },
-        {
-            id: 2,
-            title: "Fazer compras",
-            description: "29 de julho de 2025 às 14:30",
-            isCompleted: false,
-        },
-        {
-            id: 3,
-            title: "Reunião com o time",
-            description: "30 de julho de 2025 às 09:00",
-            isCompleted: true,
-        },
-        {
-            id: 4,
-            title: "Ir à academia",
-            description: "31 de julho de 2025 às 18:00",
-            isCompleted: false,
-        },
-    ]);
-    return (
-        <div className="w-screen h-screen bg-slate-400 flex items-center justify-center flex-col p-6">
-            <div className="w-[500px] bg-slate-500 py-12 px-6 rounded-xl">
-                <h1 className="text-3xl text-black font-bold text-center pb-4">Gerenciador de Tarefas</h1>
-                <Tasks tasks={tasks} />
-            </div>
-        </div>
-    );
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "28 de julho de 2025 às 10:00",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      title: "Fazer compras",
+      description: "29 de julho de 2025 às 14:30",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Reunião com o time",
+      description: "30 de julho de 2025 às 09:00",
+      isCompleted: true,
+    },
+    {
+      id: 4,
+      title: "Ir à academia",
+      description: "31 de julho de 2025 às 18:00",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(TaskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === TaskId)
+        return {
+          ...tasks,
+          isCompleted: !task.isCompleted,
+        };
+      else {
+        return task;
+      }
+    });
+    setTasks(newTasks);
+  }
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-400 p-6">
+      <div className="w-[500px] rounded-xl bg-slate-500 px-6 py-12">
+        <h1 className="pb-4 text-center text-3xl font-bold text-black">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
+    </div>
+  );
 }
 export default App;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,25 @@
+import { ChevronRightIcon, Trash2 } from "lucide-react";
+
 function Tasks(props) {
-    return (
-        <div>
-            <ul className="space-y-4 shadow p-4 bg-slate-400 rounded-lg">
-                {props.tasks.map((tasks) => (
-                    <li className="flex" key={tasks.id}>
-                        <button className="bg-slate-300 text-gray-700 p-2 rounded-md font-semibold w-full">
-                            {tasks.title}
-                        </button>
-                        <button>Details</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className="space-y-4 rounded-lg bg-slate-400 p-4 shadow">
+        {props.tasks.map((tasks) => (
+          <li className="flex gap-2" key={tasks.id}>
+            <button className="w-full rounded-md bg-slate-300 p-2 font-semibold text-gray-700">
+              {tasks.title}
+            </button>
+            <button className="rounded-md bg-slate-300 p-2 font-semibold text-gray-700">
+              <ChevronRightIcon />
+            </button>
+            <button className="rounded-md bg-slate-300 p-2 font-semibold text-gray-700">
+              <Trash2 />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Tasks;
