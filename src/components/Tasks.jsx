0ecaf2d82072feae2750dca1fc16f07d699ee148
Bruno Nitro5 <carecsnay1@gmail.com>
2025-07-29 14:52:19 -0300

    style: move text to left

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,6 @@
 import { ChevronRightIcon, Trash2 } from "lucide-react";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <div>
       <ul className="flex min-h-[250px] flex-col justify-center space-y-4 rounded-lg bg-slate-400 p-4 shadow">
@@ -10,7 +10,7 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
               onClick={() => {
                 onTaskClick(tasks.id);
               }}
-              className={`w-full rounded-md bg-slate-300 p-2 font-semibold text-gray-800 ${tasks.isCompleted && "text-gray-400 line-through"}`}
+              className={`w-full rounded-md bg-slate-300 p-2 text-left font-semibold text-gray-800 ${tasks.isCompleted && "text-gray-400 line-through"}`}
             >
               {tasks.title}
             </button>
